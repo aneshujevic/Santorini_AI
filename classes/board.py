67320fd4d6970145b08441a98dca86d0af4f9e56
Anes Hujević <35279526+owlhoo@users.noreply.github.com>
2019-12-07 15:27:46 +0100

    Fixed some of the functs

--- classes/board.py
+++ classes/board.py
@@ -1,16 +1,3 @@
-# returns list of valid moves be it moving a worker or building a block
-def get_valid_moves(starting_location, all_valid_moves):
-    valid_moving_moves = []
-    start_x = starting_location.x
-    start_y = starting_location.y
-
-    for x in range(3):
-        for y in range(3):
-            if [start_x + x - 1, start_y + y - 1] in all_valid_moves:
-                valid_moving_moves.append([start_x + x - 1, start_y + y - 1])
-    return valid_moving_moves
-
-
 class Board:
     def __init__(self):
         self.board = [[0 for x in range(5)] for y in range(5)]
@@ -19,28 +6,52 @@ class Board:
         text_representation = ''
         for x in range(5):
             for y in range(5):
-                text_representation += f'{self.board[x][y]}'
+                text_representation += f'{self.board[x][y]} '
             text_representation += '\n'
         return text_representation
 
     def print_board(self):
-        print(self.board)
+        print(self)
 
-    # returns a list of all moving moves
-    def get_all_valid_moves(self):
+    # returns a tuple of all valid moves
+    # could be bottleneck
+    @staticmethod
+    def get_all_valid_moves(board_state):
         list_of_moves = []
+
         for i in range(5):
             for j in range(5):
-                if self.board[i][j] == 0:
+                if board_state[i][j] != 4 and not board_state[i][j] is Builder:
                     list_of_moves.append([i, j])
-        return list_of_moves
+        return tuple(list_of_moves)
 
-    # returns a list of all valid build moves
-    # TODO: implement Builder class
-    def get_all_valid_builds(self):
-        list_of_moves = []
-        for i in range(5):
-            for j in range(5):
-                if self.board[i][j] != 3 and not self.board[i][j] is Builder:
-                    list_of_moves.append([i, j])
-        return list_of_moves
+    # returns a tuple of valid build moves
+    @staticmethod
+    def get_valid_builds(starting_location, all_valid_moves):
+        valid_building_moves = []
+        start_x = starting_location.x
+        start_y = starting_location.y
+
+        for x in range(3):
+            for y in range(3):
+                if start_x + x - 1 < 0 or start_x + x + 1 > 5 or start_y + y - 1 < 0 or start_y + y + 1 > 5:
+                    pass
+                if [start_x + x - 1, start_y + y - 1] in all_valid_moves:
+                    valid_building_moves.append([start_x + x - 1, start_y + y - 1])
+        return tuple(valid_building_moves)
+
+    # returns a tuple of valid moving moves
+    @staticmethod
+    def get_valid_moving_moves(starting_location, board_state, all_valid_moves):
+        valid_moving_moves = []
+        start_x = starting_location[0]
+        start_y = starting_location[1]
+
+        for x in range(3):
+            for y in range(3):
+                if start_x + x - 1 < 0 or start_x + x + 1 > 4 or start_y + y - 1 < 0 or start_y + y + 1 > 4:
+                    pass
+                if board_state[start_x][start_y] - board_state[start_x + x - 1][start_y + y - 1] == -1 and\
+                        [start_x + x - 1, start_y + y - 1] in all_valid_moves:
+                    valid_moving_moves.append([start_x + x - 1, start_y + y - 1])
+        return tuple(valid_moving_moves)
